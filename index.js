1f7d3c31d70e27f581517eea02deb7ad8cce04e3
aarongoen <aarongoen2.0@gmail.com>
2021-03-05 17:52:59 -0500

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function mondayWork(activity = 'go to the office') {
     return `This Monday, I will ${activity}.`
 }
 
+function wrapAdjective(visual_flair = '*') {
+    function innerFunction(param = 'special') {
+        return `You are ${param} ${visual_flair}`
+    }
+}
+
+let encouragingPromptFunction = wrapAdjective('!!!')
